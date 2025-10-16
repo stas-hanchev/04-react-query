24c904772f72cc8110ed1a6e87141b238531569f
Stanislav Hanchev <stanislav@smoothr.de>
2025-10-16 11:37:39 +0300

    refactor before pagination

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,45 +3,54 @@ import styles from './App.module.css'
 import type { Movie } from '../../types/movie';
 
 import { useState } from 'react'
-import { Toaster, toast } from "react-hot-toast";
+import { Toaster, /*toast*/ } from "react-hot-toast";
 import SearchBar from '../SearchBar/SearchBar';
 import MovieGrid from '../MovieGrid/MovieGrid';
 import Loader from '../Loader/Loader';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import MovieModal from '../MovieModal/MovieModal';
 import fetchMovies from '../../services/movieService';
+import { useQuery } from '@tanstack/react-query';
 
 function App() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const [title, setTitle] = useState<string>('');
+  // const [movies, setMovies] = useState<Movie[]>([]);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(false);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
-  const handleSearch = async (title: string) => {
-    setLoading(true);
-    setError(false);
-    setMovies([]);
-    try {
-      const data = await fetchMovies(title);
-      if (data.results.length === 0) {
-        toast.error("No movies found for your request.");
-      }
-      setMovies(data.results);
-    } catch {
-      setError(true);
-      toast.error("Error fetching movies.");
-    } finally {
-      setLoading(false);
-    }
-  }
+  // const handleSearch = async (title: string) => {
+  //   // setLoading(true);
+  //   // setError(false);
+  //   setMovies([]);
+  //   try {
+  //     const data = await fetchMovies(title);
+  //     if (data.results.length === 0) {
+  //       toast.error("No movies found for your request.");
+  //     }
+  //     setMovies(data.results);
+  //   } catch {
+  //     // setError(true);
+  //     toast.error("Error fetching movies.");
+  //   } finally {
+  //     // setLoading(false);
+  //   }
+  // }
+
+  const { data, /*error,*/ isLoading, isError, isSuccess } = useQuery({
+    queryKey: ['movies', title],
+    queryFn: () => fetchMovies(title),
+    enabled: title.trim().length > 0
+  });
+
 
   return (
     <div className={styles.app}>
       <Toaster />
-      <SearchBar onSubmit={handleSearch}></SearchBar>
-      {loading && <Loader />}
-      {error && <ErrorMessage />}
-      <MovieGrid movies={movies} onSelect={setSelectedMovie}></MovieGrid>
+      <SearchBar onSubmit={setTitle}></SearchBar>
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      {isSuccess && <MovieGrid movies={data.results} onSelect={setSelectedMovie}></MovieGrid>}
       {selectedMovie && (
         <MovieModal movie={selectedMovie} onClose={() => setSelectedMovie(null)} />
       )}
--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,16 @@ import 'modern-normalize';
 import './index.css'
 import App from './components/App/App'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 )
